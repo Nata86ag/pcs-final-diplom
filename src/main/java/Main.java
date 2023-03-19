267fb194f01dc678c67400b8e31e79b10dbed809
Nata86ag <116958072+Nata86ag@users.noreply.github.com>
2023-03-19 15:55:38 +0500

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,29 @@
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 import static java.lang.System.out;
 
 public class Main {
     private static final int PORT = 8989;
 
+
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         out.println(engine.search("бизнес"));
         try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            Gson gson = new Gson();
 
             while (true) {
-                try (Socket clientSocket = serverSocket.accept();
-                     PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                    String word = bufferedReader.readLine();
-                    printWriter.println(engine.search(word));
-                    Gson gson = new GsonBuilder().create();
-                    String jsonList = gson.toJson(engine.search(word));
-                    out.println(jsonList);
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+                    String word = in.readLine();
+                    List<PageEntry> searchResult = engine.search(word);
+                    out.println(gson.toJson(searchResult));
                 }
             }
         } catch (IOException e) {
@@ -32,4 +32,3 @@ public class Main {
         }
     }
 }
-
